f42605c610acd6752ce13d36533a7efca42c1b00
尧杰 <jiedev@163.com>
2022-10-23 16:30:41 +0800

    test

---
+++ DBSCAN.TESTS/DbscanTestData.cs
@@ -0,0 +1,55 @@
+﻿namespace Dbscan.Tests;
+
+public static class DbscanTestData
+{
+	public class SimplePoint : IPointData
+	{
+		public SimplePoint(double x, double y) =>
+			Point = new Point(x, y);
+
+		public Point Point { get; }
+	}
+
+	internal static IList<SimplePoint> Borders = new List<SimplePoint>
+	{
+		new SimplePoint(x: 0, y: 0),
+		new SimplePoint(x: -1.8, y: 0),
+		new SimplePoint(x: -2.3, y: 0),
+		new SimplePoint(x: -2.3, y: 0.5),
+		new SimplePoint(x: -2.3, y: -0.5),
+		new SimplePoint(x: 1.8, y: 0),
+		new SimplePoint(x: 2.3, y: 0),
+		new SimplePoint(x: 2.3, y: 0.5),
+		new SimplePoint(x: 2.3, y: -0.5),
+	};
+
+	internal static List<SimplePoint> BuildRingDataset(int rowCount = 6, int colCount = 5)
+	{
+		var points = new List<SimplePoint>();
+
+		var rows = rowCount;
+		var cols = colCount;
+		for (var row = 0; row < rows; row++)
+		{
+			for (var col = 0; col < cols; col++)
+			{
+				var minPoints = row;
+				var eps = 1.25 - col * 0.25;
+
+				var x0 = -15 + (30 / (rows + 1)) * (row + 1);
+				var y0 = -12 + (24 / (cols + 1)) * (col + 1);
+
+				points.Add(new SimplePoint(x: x0, y: y0));
+				for (var i = 0; i < minPoints; i++)
+				{
+					var x = x0 + eps * Math.Sin(2 * Math.PI * i / minPoints);
+					var y = y0 + eps * Math.Cos(2 * Math.PI * i / minPoints);
+					points.Add(new SimplePoint(x: x, y: y));
+				}
+			}
+		}
+		return points;
+	}
+
+	internal static IList<SimplePoint> RingDataset = BuildRingDataset();
+}
---
+++ DBSCAN.TESTS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Dbscan.RBush;
+using Spectre.Console;
+
+namespace Dbscan.Tests;
+
+internal class Program
+{
+	public static void Main(string[] args)
+	{
+		AnsiConsole.Write(new FigletText("DBSCAN TEST").Centered().Color(Color.Red));
+		AnsiConsole.Write(new Rule($"[White] {DateTime.Now:yyyy-MM-dd hh:mm:ss} Create Data [/]").Centered());
+		var data = DbscanTestData.BuildRingDataset(10, 50);
+		AnsiConsole.Write(new Rule().Centered());
+		AnsiConsole.Write(new Rule($"[White] {DateTime.Now:yyyy-MM-dd hh:mm:ss} Create data count {data.Count}，Calculate Clusters [/]").Centered());
+		var st = Stopwatch.StartNew();
+		var clusters = DbscanRBush.CalculateClusters(data, 1.3, 4);
+
+		var index = 0;
+		foreach (var item in clusters.Clusters)
+		{
+			Console.WriteLine($"[White] Cluster Index : {index++} : {item.Objects?.Count} [/]");
+		}
+
+		AnsiConsole.Write(new Rule().Centered());
+		AnsiConsole.Write(new Rule($"[White] Times ${st.Elapsed.Seconds} s, Calculated. ClusterCount : {clusters.Clusters.Count} UnclusteredCount :{clusters.UnclusteredObjects.Count}[/]").Centered());
+	}
+}
